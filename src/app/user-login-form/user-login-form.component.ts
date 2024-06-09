bb56b5f772b3942e11e6d0811fa1d1ae5f771dd3
atmackenzie51 <126356439+atmackenzie51@users.noreply.github.com>
2024-06-08 20:25:56 -0400

    Update user-login-form.component.ts

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -3,32 +3,39 @@ import { UserRegistrationService } from '../fetch-api-data.service';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
+interface UserData {
+  Username: string;
+  Password: string;
+}
+
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
   styleUrl: './user-login-form.component.scss'
 })
 export class UserLoginFormComponent implements OnInit {
-  @Input() userData = { Username: "", Password: "" };
+  @Input() userData: UserData = { Username: "", Password: "" };
 
   constructor(
     public fetchApiData: UserRegistrationService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar,
+    public snackBar: MatSnackBar
   ) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    console.log(this.userData);
+  }
 
   logInUser(): void {
-    this.fetchApiData.userLogin(this.userData).subscribe((response) => {
+    this.fetchApiData.userLogin(this.userData).subscribe((result) => {
+      localStorage.setItem('user', JSON.stringify(this.userData.Username));
+      localStorage.setItem('token', result.token);
+      console.log(result);
       this.dialogRef.close(); // This will close the modal on success!
-      console.log(response);
       this.snackBar.open(`Login successful.`, "OK", {
         duration: 2000
       });
-      localStorage.setItem('user', JSON.stringify(response.user));
-      localStorage.setItem('token', response.token);
-      console.log(response.user);
+
     }, (response) => {
       console.log(response);
       this.snackBar.open("Login unsuccessful", "OK", {
